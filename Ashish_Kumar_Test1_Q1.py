ae462c97c85ffea5853c7e101a11abc91594b582
Akumar4016 <92951213+Akumar4016@users.noreply.github.com>
2021-10-22 18:17:17 +0300

    Add files via upload

---
+++ Ashish_Kumar_Test1_Q1.py
@@ -0,0 +1,9 @@
+#Variable
+
+user_input = input("Enter Your First Name : ")
+
+#Syntax For Output With Converted And Non-Converted Text
+
+print("Normal Text : " , user_input)
+
+print("Converted Text : " , str.swapcase(user_input))
